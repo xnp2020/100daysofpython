a2578476744dac14c833251d1851ed2fc0ccef5a
xianianping <xianianping@tongfutech.cn>
2022-06-10 20:03:02 +0800

    ddd

---
+++ 办公/test_excel.py
@@ -0,0 +1,12 @@
+import datetime
+
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.active
+
+ws['A1'] = 42
+ws.append([1, 2, 3])
+ws['A2'] = datetime.datetime.now()
+
+wb.save("sample.xlsx")
