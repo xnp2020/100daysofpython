11b52886c0ecef011bdfc503da49f78ef8e4c778
xianianping <xianianping@tongfutech.cn>
2022-07-05 16:49:07 +0800

    0705

--- django_mysite/mysite/polls/models.py
+++ django_mysite/mysite/polls/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(verbose_name='出版日期')
 
     @admin.display(
         boolean=True,
@@ -25,7 +25,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField('选项内容')
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
--- django_study/proj1/app1/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def show_index(request):
-    return HttpResponse('<h1>Hello, Django!</h1>')
